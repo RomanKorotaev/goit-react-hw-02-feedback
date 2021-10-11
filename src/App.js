aa4f5f166e5a2a4f834c7167867d7d3e16790903
Roman Korotaev <kro1986@ukr.net>
2021-10-11 23:18:11 +0300

    Исправлен общий обработчик

--- src/App.js
+++ src/App.js
@@ -12,32 +12,15 @@ class App extends Component {
     bad: 0
 }
 
- handleClickGood = e => {
-        // this.setState({
-        //     [e.currentTarget.name]: e.currentTarget.value
-        // });
-   
-    // this.setState(prevState => ({
-    //       //  [e.currentTarget.name]:  prevState.[e.currentTarget.name] +1
-    //  [e.currentTarget.name]: prevState.good + 1
-    //      }))
-   this.setState(prevState => ({
-     good: prevState.good + 1
-         }))
- }
 
-  handleClickNeutral = e => {
-   this.setState(prevState => ({
-    neutral: prevState.neutral + 1
-         })) 
- }
+// Общий обработчик для нескольких разных кнопок
+  handleFeedback = (e) => {
+    const { name } = e.currentTarget;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
   
-  
- handleClickBad = e => {
-   this.setState(prevState => ({
-    bad: prevState.bad + 1
-         })) 
- }
 
   // Подсчитываем общую сумму
   totalFeedback = () => {
@@ -45,10 +28,10 @@ class App extends Component {
     return (good + neutral + bad);
   };
 
+
 // Высчитываем пропрорцию (процент) позитивных откликов
   positiveFeedbackPercentage = () => {
     return Math.round(this.state.good * 100 / this.totalFeedback());
-
   }
 
 
@@ -56,33 +39,18 @@ class App extends Component {
   render() {
 
     const { good, neutral, bad } = this.state;
-    const { totalFeedback, positiveFeedbackPercentage } = this;
-     
-        // let totalFeedback = this.state.good + this.state.neutral + this.state.bad;
-        // let positiveFeedbackPercentage = Math.round(100*good/totalFeedback)
-
-   
+    const { handleFeedback, totalFeedback, positiveFeedbackPercentage } = this;
+        
     return (
 
       <div>
           <h1>Please leave feedback</h1>
           
         <div>
-          <button type="button" name="good" onClick={this.handleClickGood}> Good </button>
-            <button type="button"  name="neutral" onClick={this.handleClickNeutral}> Neutral </button>
-            <button type="button"  name="bad" onClick={this.handleClickBad}> Bad </button>
-
-           {/* <button type="button"  value="good" name="good" onClick={this.increment}> Good </button>
-            <button type="button" value="neutral" onClick={this.increment}> Neutral </button>
-            <button type="button" value="bad" onClick={this.increment}> Bad </button> */}
+            <button type="button" name="good" onClick={this.handleFeedback}> Good </button>
+            <button type="button"  name="neutral" onClick={this.handleFeedback}> Neutral </button>
+            <button type="button"  name="bad" onClick={this.handleFeedback}> Bad </button>
           </div>
-
-        <h2>Statistics</h2>
-        <p>Good:  { good }</p>
-        <p>Neutral: { neutral }</p>
-        <p>Bad: { bad }</p>
-        <p>Total: {totalFeedback()} </p>
-        <p>Positive feedback: {positiveFeedbackPercentage()} % </p>
 
         <Statistics
           good={good}
